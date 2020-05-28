d3910f453ac804f3e409bb0542c89b9129ef5c44
Nemanja Tanaskovic <nemanjatanaskovic.etf@gmail.com>
2020-05-28 10:16:20 +0200

    Adding Panel Access to NovaServiceProvider.

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -42,9 +42,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return in_array($user->email, [
-                'nemanjatanaskovic.eng@gmail.com'
-            ]);
+            return $user->permissions()->contains('can_access_panel');
         });
     }
 
